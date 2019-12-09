91d2b90c3539f8dc964ff8d03c7c07bf3ada6354
mariegiannoni <52198581+mariegiannoni@users.noreply.github.com>
2019-12-09 21:56:25 +0100

    add openConnection method

--- src/interfaceFXML/Main.java
+++ src/interfaceFXML/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import main.java.CinemaSpaceArchive;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
@@ -13,6 +14,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage){
 		try {
+			CinemaSpaceArchive.openConnection("127.0.0.1","27017");
 			Parent root = FXMLLoader.load(getClass().getResource("connection.fxml"));
 			Scene sceneConnection = new Scene(root);
 			stage.setTitle("CinemaSpace");
@@ -27,4 +29,6 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
+
 
